ea245ad77536161ae5a5b8c142f21f116dca593f
Cyprien Leschi <cyprien.leschi@gmail.com>
2022-02-11 10:51:31 +0100

    feat : fixed code and splitted wordlist

--- src/hello.cu
+++ src/hello.cu
@@ -3,15 +3,41 @@
 #include <cuda_runtime.h>
 #include <cuda.h>
 
-__global__ void cuda_hello(int id){
-	printf("Hello World du GPU %d\n", id);
-}
+// __global__ void cuda_hello(int id){
+// 	printf("Hello World du GPU %d\n", id);
+// }
 
-int main(void) {
-	printf("Hello World du CPU\n");
-    for (int i=0;i<10;i++) {
-	    cuda_hello<<<2,5>>>(i);
-    }
-    cudaDeviceSynchronize();
+int main(int argc, char **argv) {
+	if (argc<2) {
+		printf("Usage: %s <file>", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	FILE *shadow_fd = fopen(argv[1], "r");
+	char shadow_db[5000][50];
+
+	if (shadow_fd == NULL)
+		exit(EXIT_FAILURE);
+
+	int line = 0;
+
+	while ((fgets(shadow_db[line], 50, shadow_fd)) != NULL)
+	{
+		shadow_db[line][strlen(shadow_db[line]) - 1] = '\0';
+		printf("address:%p -> %s\n", shadow_db[line], shadow_db[line]);
+		line++;
+	}
+
+	printf("[DEBUG] Shadow content - head : \n");
+	for (int i = 0; i < 10; i++)
+	{
+		printf("[%i] : %s\n", i, shadow_db[i]);
+	}
+
+	// printf("Hello World du CPU\n");
+    // for (int i=0;i<10;i++) {
+	//     cuda_hello<<<2,5>>>(i);
+    // }
+    // cudaDeviceSynchronize();
 	return EXIT_SUCCESS;
 }
--- src/multiattack.cu
+++ src/multiattack.cu
@@ -7,53 +7,36 @@
 #include <sys/types.h>
 #include <string.h>
 
-#define DEBUG 0
+#define DEBUG true
 #define WL_BLOCK 1000
-#define MAX_LINE_SIZE 50
+#define MAX_LINE_LENGTH 200
 #define MAX_SHADOW_LENGTH 5000
 #define MAX_HASH_LENGTH 50
 
-// Default maximum number of simultaneous process
-int MAX_FILS = 5;
-
-// wraup for readline
-char *readline(FILE *f)
-{
-	char *line = NULL;
-
-	size_t len = 0;
-	ssize_t read;
-	if ((read = getline(&line, &len, f)) != -1)
-	{
-		line[read - 2] = '\0';
-
-		return line;
-	}
-	return NULL;
-}
-
-__global__ void check_hash(char **wordlist_block, int lines, char **shadow_db)
+__global__ void check_hash(char **wordlist_block_plain, char **wordlist_block_hash, int lines, char **shadow_db, int shadow_count)
 {
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
-	// printf("%p index=%d\n", wordlist_block, index);
-	char *current_hash = shadow_db[index];
+	if (blockIdx.x==0 && index < shadow_count) {
+		char *current_hash = shadow_db[index];
 
-	printf("[Block - %d] Cracking hash %s (%d)\n", current_hash, wordlist_block[index], index);
-	// for (int i = 0; i < lines; i++)
-	// {
-	// 	bool ok = true;
-	// 	for (int v = 0; v < MAX_HASH_LENGTH; v++)
-	// 	{
-	// 		if (current_hash[v] == '\0' || wordlist_block_hash[i][v] == '\0' || current_hash[v] != wordlist_block_hash[i][v])
-	// 		{
-	// 			ok = false;
-	// 			break;
-	// 		}
-	// 	}
-	// 	if (ok)
-	// 		printf("[+] FOUND %s\n", wordlist_block_plain);
-	// 	break;
-	// }
+		printf("[Thread - (%d,%d)] Cracking hash %s (%d)\n", blockIdx.x, threadIdx.x, current_hash, index);
+		for (int i = 0; i < lines; i++)
+		{
+			printf("[Thread - (%d,%d)] Testing '%s' (%s)\n", blockIdx.x, threadIdx.x, wordlist_block_plain[i], wordlist_block_hash[i]);
+		// 	bool ok = true;
+		// 	for (int v = 0; v < MAX_HASH_LENGTH; v++)
+		// 	{
+		// 		if (current_hash[v] == '\0' || wordlist_block_hash[i][v] == '\0' || current_hash[v] != wordlist_block_hash[i][v])
+		// 		{
+		// 			ok = false;
+		// 			break;
+		// 		}
+		// 	}
+		// 	if (ok)
+		// 		printf("[+] FOUND %s\n", wordlist_block_plain);
+		// 	break;
+		}
+	}
 }
 
 // bool check_hash(const char *hash, char **wordlist_block, int wordlist_block_size)
@@ -77,59 +60,91 @@ __global__ void check_hash(char **wordlist_block, int lines, char **shadow_db)
 
 int main(int argc, char *argv[])
 {
+	// parsing arguments
 	if (argc < 3)
 		fprintf(stderr, "Usage: '%s' dictionnary_file shasum_file\n", argv[0]), exit(EXIT_FAILURE);
-	// int M = strtol(argv[1], NULL, 10);
-	// int T = strtol(argv[2], NULL, 10);
-	char *dict_file = argv[3];
-	char *shasum_file = argv[4];
+	char *dict_file = argv[1];
+	char *shasum_file = argv[2];
 
 	// opening files
-	FILE *shadow_fd = fopen(shasum_file, "r");
-	FILE *wordlist_fd = fopen(dict_file, "r");
-	if (shadow_fd == NULL || wordlist_fd == NULL)
+	FILE *shadow_file = fopen(shasum_file, "r");
+	FILE *wordlist_file = fopen(dict_file, "r");
+	if (shadow_file == NULL || wordlist_file == NULL) {
+		printf("Error while opening %s file\n", shadow_file == NULL ? "shadow" : "wordlist");
 		exit(EXIT_FAILURE);
-
-	/* ------------- Loading shadow db into device ------------- */
-	char shadow_db[MAX_SHADOW_LENGTH][MAX_LINE_SIZE];
-	char **shadow_dbGPU;
-	char *line = NULL;
-	size_t len = 0;
-	int shadow_count = 0;
-
-	while ((getline(&line, &len, shadow_fd)) != -1)
-	{
-		// sprintf(shadow_db[i], "%s", line);
-		strcpy(shadow_db[shadow_count], line);
-		shadow_count++;
 	}
 
-	cudaMallocManaged(shadow_dbGPU, MAX_SHADOW_LENGTH * MAX_LINE_SIZE * sizeof(char));
-	cudaMemcpy(shadow_dbGPU, shadow_db, MAX_SHADOW_LENGTH * MAX_LINE_SIZE * sizeof(char), cudaMemcpyHostToDevice);
+
+	/* ------------- Loading shadow db into device ------------- */
+	char *shadow_db[MAX_SHADOW_LENGTH];
+	char **shadow_dbGPU;
+	char buf[MAX_LINE_LENGTH];
+
+	int shadow_count = 0;
+
+	while ((fgets(buf, MAX_LINE_LENGTH, shadow_file)) != NULL)
+	{
+		buf[strlen(buf) - 1] = '\0'; // remove the trailing newline
+		#if DEBUG
+			// printf("address:%p -> %s\n", buf, buf);
+		#endif
+		cudaMallocManaged(&shadow_db[shadow_count], strlen(buf));
+		cudaMemcpy(shadow_db[shadow_count], buf, strlen(buf), cudaMemcpyHostToDevice);
+		shadow_count++;
+	}	
+
+	cudaMallocManaged(&shadow_dbGPU, shadow_count * sizeof(char *));
+	cudaMemcpy(shadow_dbGPU, shadow_db, shadow_count * sizeof(char *), cudaMemcpyHostToDevice);
+
+	// #if DEBUG
+	// 	printf("[DEBUG] Shadow content - head : \n");
+	// 	for (int i = 0; i < 10; i++)
+	// 	{
+	// 		printf("[%i] : %s\n", i, shadow_db[i]);
+	// 	}
+	// #endif
+
 
 	/* ------- Optimizing number of threads & blocks based on 0.5 ratio ------ */
 	int M = ceil((double)shadow_count / sqrt((shadow_count / 0.5)));
 	int T = ceil((double)shadow_count / (double)M);
-	// printf("[DEBUG] Shadow content - head : \n");
-	// for (int i = 0; i < 10; i++)
-	// {
-	// 	printf("[%i] : %s\n", i, shadow_db[i]);
-	// }
 
-	/* ------------- Creating first parrallelisation by dividing wordlist (divide&conquer strategy) ------------- */
+	#if DEBUG
+		printf("[DEBUG] Computed values : M=%d ; T=%d\n", M, T);
+	#endif
+
+	/* ------------- Creating first parrallelisation by dividing wordlist into several blocks (divide & conquer strategy) ------------- */
 
 	int block_counter = 0;
 	while (true)
 	{
 		size_t lines = 0; /** next index to be used with lineBuffer
 					(and number of lines already stored)*/
-		char *lineBuffer[WL_BLOCK];
-		char buf[MAX_LINE_SIZE];
-		while (lines < WL_BLOCK && fgets(buf, sizeof(buf), wordlist_fd) != NULL)
+		char *lineBuffer_plain[WL_BLOCK];
+		char *lineBuffer_hash[WL_BLOCK];
+		char buf[MAX_LINE_LENGTH];
+		while (lines < WL_BLOCK && fgets(buf, sizeof(buf), wordlist_file) != NULL)
 		{
-			buf[strlen(buf) - 1] = '\0';
-			cudaMallocManaged(&lineBuffer[lines], strlen(buf) * sizeof(char));
-			cudaMemcpy(lineBuffer[lines], buf, strlen(buf), cudaMemcpyHostToDevice);
+			char *plain, *hash;
+			buf[strlen(buf) - 1] = '\0'; // remove trailing newline
+			for (int v = 0; v < MAX_LINE_LENGTH; v++) {
+				if (buf[v] == '\0') {
+					printf("ERROR: invalid input line \"%s\" in wordlist\n", buf);
+					exit(EXIT_FAILURE);
+				}
+				if (buf[v] == ' ') {
+					buf[v] = '\0';
+					plain = buf;
+					hash = buf + v + 1;
+					break;
+				}
+			}
+
+			cudaMallocManaged(&lineBuffer_plain[lines], strlen(plain) * sizeof(char));
+			cudaMemcpy(lineBuffer_plain[lines], plain, strlen(plain), cudaMemcpyHostToDevice);
+
+			cudaMallocManaged(&lineBuffer_hash[lines], strlen(hash) * sizeof(char));
+			cudaMemcpy(lineBuffer_hash[lines], hash, strlen(hash), cudaMemcpyHostToDevice);
 			lines++;
 		}
 		if (lines == 0)
@@ -138,10 +153,14 @@ int main(int argc, char *argv[])
 		block_counter++;
 		printf("[+] Assigned block %d (read %zd lines)\n", block_counter, lines);
 
-		char **lineBufferGPU;
-		cudaMallocManaged(&lineBufferGPU, lines * MAX_LINE_SIZE * sizeof(char));
-		cudaMemcpy(lineBufferGPU, lineBuffer, sizeof(char *) * lines, cudaMemcpyHostToDevice);
-		check_hash<<<M, T>>>(lineBufferGPU, lines, shadow_dbGPU);
+		char **lineBuffer_plainGPU, **lineBuffer_hashGPU;
+		cudaMallocManaged(&lineBuffer_plainGPU, lines * sizeof(char *));
+		cudaMemcpy(lineBuffer_plainGPU, lineBuffer_plain, lines * sizeof(char *), cudaMemcpyHostToDevice);
+
+		cudaMallocManaged(&lineBuffer_hashGPU, lines * sizeof(char *));
+		cudaMemcpy(lineBuffer_hashGPU, lineBuffer_hash, lines * sizeof(char *), cudaMemcpyHostToDevice);
+
+		check_hash<<<M, T>>>(lineBuffer_plainGPU, lineBuffer_hashGPU, lines, shadow_dbGPU, shadow_count);
 
 		// for (int i = 0; i < lines; i++)
 		// {
