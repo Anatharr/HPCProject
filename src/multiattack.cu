464588296cac411fcf8d770e97251d7922903099
spicytelescope <jbeziaud@gmail.com>
2022-02-11 14:03:57 +0100

    feat: adding visual loading + helpers

--- src/multiattack.cu
+++ src/multiattack.cu
@@ -13,6 +13,10 @@
 #define MAX_SHADOW_LENGTH 5000
 #define MAX_HASH_LENGTH 50
 
+// Progress bar
+#define PBSTR "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||"
+#define PBWIDTH 60
+
 __global__ void check_hash(char **wordlist_block_plain, char **wordlist_block_hash, int lines, char **shadow_db, int shadow_count)
 {
 	int index = blockIdx.x * blockDim.x + threadIdx.x;
@@ -20,14 +24,14 @@ __global__ void check_hash(char **wordlist_block_plain, char **wordlist_block_ha
 	{
 		char *current_hash = shadow_db[index];
 
-		#if DEBUG
-			printf("[Thread - (%d,%d)] Cracking hash %s (%d)\n", blockIdx.x, threadIdx.x, current_hash, index);
-		#endif
+#if DEBUG
+		printf("[Thread - (%d,%d)] Cracking hash %s (%d)\n", blockIdx.x, threadIdx.x, current_hash, index);
+#endif
 		for (int i = 0; i < lines; i++)
 		{
-			#if DEBUG
-				printf("[Thread - (%d,%d)] Testing '%s' (%s)\n", blockIdx.x, threadIdx.x, wordlist_block_plain[i], wordlist_block_hash[i]);
-			#endif
+#if DEBUG
+			printf("[Thread - (%d,%d)] Testing '%s' (%s)\n", blockIdx.x, threadIdx.x, wordlist_block_plain[i], wordlist_block_hash[i]);
+#endif
 			bool ok = true;
 			for (int v = 0; v < MAX_HASH_LENGTH; v++)
 			{
@@ -39,7 +43,8 @@ __global__ void check_hash(char **wordlist_block_plain, char **wordlist_block_ha
 					break;
 				}
 			}
-			if (ok) {
+			if (ok)
+			{
 				printf("\u001b[32m[+] FOUND \u001b[33;1m%s\u001b[0;32m for hash %s (shadow_index = %d)\n\u001b[0m", wordlist_block_plain[i], current_hash, index);
 				break;
 			}
@@ -49,28 +54,86 @@ __global__ void check_hash(char **wordlist_block_plain, char **wordlist_block_ha
 
 // float speedUp(float T, )
 
+int countlines_from_fp(FILE *fp)
+{
+	// count the number of lines in the file called filename
+	int ch = 0;
+	int lines = 0;
+
+	if (fp == NULL)
+	{
+		printf("ERROR OPENING FILE - proceeding");
+		fseek(fp, 0, SEEK_SET);
+		return 0;
+	}
+
+	lines++;
+	while ((ch = fgetc(fp)) != EOF)
+	{
+		if (ch == '\n')
+			lines++;
+	}
+	fseek(fp, 0, SEEK_SET);
+	return lines;
+}
+
+void updatePBar(int current_block, int total_num_of_block)
+{
+	double percentage = (double)current_block / total_num_of_block;
+	int pb_width = strlen(PBSTR);
+	int lpad = (int)(percentage * pb_width);
+	int rpad = pb_width - lpad;
+	printf("\r\x1b[33;1m Testing wordlist blocks ... [%d/%d] [%.*s%*s]\x1b[0m", current_block, total_num_of_block, lpad, PBSTR, rpad, "");
+	fflush(stdout);
+}
+
 int main(int argc, char *argv[])
 {
+	bool DISABLE_PBAR = false;
+	double M_T_RATIO = 0.5;
+
 	// parsing arguments
+	bool error_flag = false;
 	if (argc < 3)
+		error_flag = true;
+	else if (argc == 4)
+	{
+		sscanf(argv[3], "%lf", &M_T_RATIO);
+	}
+	else if (argc == 5)
+	{
+		DISABLE_PBAR = (bool)atoi(argv[4]);
+	}
+	else if (argc > 5)
+	{
+		error_flag = true;
+	}
+	if (error_flag)
 		fprintf(stderr, "Usage: '%s' dictionnary_file shasum_file\n", argv[0]), exit(EXIT_FAILURE);
 	char *dict_file = argv[1];
 	char *shasum_file = argv[2];
 
-	// opening files
+	/* ------------- Opening wordlist and shadow file ------------- */
 	FILE *shadow_file = fopen(shasum_file, "r");
 	FILE *wordlist_file = fopen(dict_file, "r");
+	int wordlist_n_lines = 0, total_num_of_block = 0;
+
+	if (DISABLE_PBAR == false)
+	{
+		wordlist_n_lines = countlines_from_fp(wordlist_file);
+		total_num_of_block = (wordlist_n_lines / WL_BLOCK) + 1;
+	}
+
 	if (shadow_file == NULL || wordlist_file == NULL)
 	{
-		printf("Error while opening %s file\n", shadow_file == NULL ? "shadow" : "wordlist");
-		exit(EXIT_FAILURE);
+		printf("Error while opening %s file\n", shadow_file == NULL ? "shadow" : "wordlist"),
+			exit(EXIT_FAILURE);
 	}
 
 	/* ------------- Loading shadow db into device ------------- */
 	char *shadow_db[MAX_SHADOW_LENGTH];
 	char **shadow_dbGPU;
 	char buf[MAX_LINE_LENGTH];
-
 	int shadow_count = 0;
 
 	while ((fgets(buf, MAX_LINE_LENGTH, shadow_file)) != NULL)
@@ -95,8 +158,8 @@ int main(int argc, char *argv[])
 	// 	}
 	// #endif
 
-	/* ------- Optimizing number of threads & blocks based on 0.5 ratio ------ */
-	int M = ceil((double)shadow_count / sqrt((shadow_count / 0.5)));
+	/* ------- Optimizing number of threads & blocks ------ */
+	int M = ceil((double)shadow_count / sqrt((shadow_count / M_T_RATIO)));
 	int T = ceil((double)shadow_count / (double)M);
 
 #if DEBUG
@@ -108,6 +171,9 @@ int main(int argc, char *argv[])
 	int block_counter = 0;
 	while (true)
 	{
+		if (DISABLE_PBAR == false)
+			updatePBar(block_counter, total_num_of_block);
+
 		size_t lines = 0; /** next index to be used with lineBuffer
 					(and number of lines already stored)*/
 		char *lineBuffer_plain[WL_BLOCK];
